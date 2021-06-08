9e851f8c94eeb33069c920158f543abff53017e8
Mauricio Robayo <rfmajo@gmail.com>
2021-06-08 12:55:33 -0500

    chore: update randomPromise funciton to take bibleId
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- functions/src/promises/randomPromise.ts
+++ functions/src/promises/randomPromise.ts
@@ -6,80 +6,82 @@ import {
   getRandomPromises,
   getMongoDbCollection,
 } from "../helpers";
+import {BibleId} from "../types";
 
-const BIBLE_ID = "kjv";
 const bibleSuperSearch = new BibleSuperSearch();
 
 admin.initializeApp();
 
-export const randomPromise = functions.https.onCall(async () => {
-  const promisesCollection = await getMongoDbCollection("promises");
-  const randomPromises = await getRandomPromises(100);
-  try {
-    for (const randomPromise of randomPromises) {
-      const humanReadableReference = osisToHumanReadableReference(
-        randomPromise.osis
-      );
-
-      if (!humanReadableReference) {
-        await promisesCollection.updateOne(
-          {_id: randomPromise._id},
-          {
-            $set: {
-              errors: [
-                `osisToHumanReadableReference failed on '${randomPromise.osis}'`,
-              ],
-            },
-          }
+export const randomPromise = functions.https.onCall(
+  async ({bibleId}: {bibleId: BibleId}) => {
+    const promisesCollection = await getMongoDbCollection("promises");
+    const randomPromises = await getRandomPromises(100);
+    try {
+      for (const randomPromise of randomPromises) {
+        const humanReadableReference = osisToHumanReadableReference(
+          randomPromise.osis
         );
-        continue;
-      }
 
-      if (randomPromise.content?.[BIBLE_ID]) {
-        return {
-          text: randomPromise.content[BIBLE_ID].text,
-          reference: randomPromise.content[BIBLE_ID].reference,
-          source: randomPromise.source,
-        };
-      }
-
-      const response = await bibleSuperSearch.getPassageFromReference(
-        BIBLE_ID,
-        humanReadableReference
-      );
-
-      if (response.status === "success") {
-        await promisesCollection.updateOne(
-          {_id: randomPromise._id},
-          {
-            $set: {
-              [`content.${BIBLE_ID}`]: {
-                text: response.text,
-                reference: humanReadableReference,
+        if (!humanReadableReference) {
+          await promisesCollection.updateOne(
+            {_id: randomPromise._id},
+            {
+              $set: {
+                errors: [
+                  `osisToHumanReadableReference failed on '${randomPromise.osis}'`,
+                ],
               },
+            }
+          );
+          continue;
+        }
+
+        if (randomPromise.content?.[bibleId]) {
+          return {
+            text: randomPromise.content[bibleId].text,
+            reference: randomPromise.content[bibleId].reference,
+            source: randomPromise.source,
+          };
+        }
+
+        const response = await bibleSuperSearch.getPassageFromReference(
+          bibleId,
+          humanReadableReference
+        );
+
+        if (response.status === "success") {
+          await promisesCollection.updateOne(
+            {_id: randomPromise._id},
+            {
+              $set: {
+                [`content.${bibleId}`]: {
+                  text: response.text,
+                  reference: humanReadableReference,
+                },
+              },
+            }
+          );
+          return {
+            text: response.text,
+            reference: humanReadableReference,
+            source: randomPromise.source,
+          };
+        }
+
+        await promisesCollection.updateOne(
+          {_id: randomPromise._id},
+          {
+            $set: {
+              errors: response.errors,
             },
           }
         );
-        return {
-          text: response.text,
-          reference: humanReadableReference,
-          source: randomPromise.source,
-        };
       }
-
-      await promisesCollection.updateOne(
-        {_id: randomPromise._id},
-        {
-          $set: {
-            errors: response.errors,
-          },
-        }
-      );
+    } catch (err) {
+      functions.logger.error(`randomPromise error: ${JSON.stringify(err)}`);
+      throw new functions.https.HttpsError("internal", "randomPromise error");
     }
-  } catch (err) {
-    functions.logger.error(`randomPromise error: ${JSON.stringify(err)}`);
-    throw new functions.https.HttpsError("internal", "randomPromise error");
+    functions.logger.error("Could not get a random promise!");
+    throw new functions.https.HttpsError("internal", "randomPromise failed");
   }
-  functions.logger.error("Could not get a random promise!");
-  throw new functions.https.HttpsError("internal", "randomPromise failed");
-});
+);
