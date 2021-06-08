d6e80db2bc36544e0811ebf6a5f4fd22f1b8bc51
Mauricio Robayo <rfmajo@gmail.com>
2021-06-08 15:12:04 -0500

    chore: disable selected language option
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/src/features/i18next/LanguageSelector.tsx
+++ client/src/features/i18next/LanguageSelector.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link, useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const Wrapper = styled.div`
   margin: 1rem auto;
@@ -18,10 +19,14 @@ const langs: Record<string, { nativeName: string }> = {
 
 const LanguageSelector = () => {
   const { pathname } = useLocation();
+  const { i18n } = useTranslation();
   return (
     <Wrapper>
       {Object.keys(langs).map((lang) => {
         const [, , ...path] = pathname.split("/");
+        if (i18n.language === lang) {
+          return <span>{langs[lang].nativeName}</span>;
+        }
         return (
           <Link key={lang} to={`/${lang}/${path.join("/")}`}>
             {langs[lang].nativeName}
