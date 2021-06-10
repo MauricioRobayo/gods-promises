7ef80f4cdbd4f7cbac38b832f3b689a317d273af
Mauricio Robayo <rfmajo@gmail.com>
2021-06-10 06:40:13 -0500

    chore: remove comment
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- functions/src/GPromises/getGPromise.ts
+++ functions/src/GPromises/getGPromise.ts
@@ -3,9 +3,6 @@ import {GPromiseDTO} from "../models/GPromise";
 import {HttpsError} from "firebase-functions/lib/providers/https";
 import {getGPromiseById} from "../queries";
 
-// TODO: Move humanReadableReference to scrappers, when we get the data. Avoids an extra step here, avoids bundling osisToEn package, and avoids inserting invalid docs.
-// TODO: Include bible name on Content.
-
 export const getGPromise = functions.https.onCall(
   async ({gPromiseId}: {gPromiseId: string}): Promise<GPromiseDTO> => {
     const gPromise = await getGPromiseById(gPromiseId);
