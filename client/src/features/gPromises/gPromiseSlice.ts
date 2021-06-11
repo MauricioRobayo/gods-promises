eed62ec891b2e7331155445552115e00f18c7431
Mauricio Robayo <rfmajo@gmail.com>
2021-06-11 08:52:12 -0500

    chore: create initial gPromiseSlice
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ client/src/features/gPromises/gPromiseSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {},
+  reducers: {},
+});
+
+export default counterSlice.reducer;
