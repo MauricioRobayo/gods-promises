356f78c7e71cdf79ae1cb4a53ebb9298f55e76f5
Mauricio Robayo <rfmajo@gmail.com>
2021-06-11 07:39:05 -0500

    chore: remove unnecessary logic from useRandomPromise
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/src/hooks/useRandomGPromise.ts
+++ client/src/hooks/useRandomGPromise.ts
@@ -1,15 +1,8 @@
 import axios from "axios";
-import { useEffect } from "react";
-import { useTranslation } from "react-i18next";
 import { useQuery, useQueryClient } from "react-query";
-import { useHistory } from "react-router-dom";
 
 export default function useRandomGPromise() {
   const queryClient = useQueryClient();
-  const { push } = useHistory();
-  const {
-    i18n: { language },
-  } = useTranslation();
 
   async function randomGPromise() {
     const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/random`);
@@ -23,13 +16,6 @@ export default function useRandomGPromise() {
       staleTime: Infinity,
     }
   );
-
-  useEffect(() => {
-    if (isLoading || isError) {
-      return;
-    }
-    push(`/${language}/p/${data.id}`);
-  }, [push, language, data, isLoading, isError]);
 
   return {
     data,
