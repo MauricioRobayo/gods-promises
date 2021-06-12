40dae2b51c17216d9df439b604848c4681e0ee02
Mauricio Robayo <rfmajo@gmail.com>
2021-06-11 22:00:12 -0500

    chore: apply stash
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/src/styles/index.ts
+++ client/src/styles/index.ts
@@ -1,16 +1,2 @@
-import { createGlobalStyle } from "styled-components";
-import { normalize } from "styled-normalize";
-
-export const GlobalStyle = createGlobalStyle`
-  ${normalize}
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    color: #444;
-    font-family: Lato, serif;
-  }
-`;
+export * from "./global";
+export * from "./theme";
