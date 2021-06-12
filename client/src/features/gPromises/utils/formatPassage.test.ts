cf6f1c84464bdd8d9865b72f43f6d02a2d691a5a
Mauricio Robayo <rfmajo@gmail.com>
2021-06-12 11:08:22 -0500

    feat(#4): add formatPassage function
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ client/src/features/gPromises/utils/formatPassage.test.ts
@@ -0,0 +1,19 @@
+import { formatPassage } from "./formatPassage";
+
+describe("cleanPassage", () => {
+  const cases = [
+    ["Some passage.", "Some passage."],
+    ["Some passage?", "Some passage?"],
+    ["Some passage!", "Some passage!"],
+    ["some passage!", "…some passage!"],
+    ["some passage.", "…some passage."],
+    ["Some passage", "Some passage…"],
+    ["some passage", "…some passage…"],
+    ["some passage;", "…some passage…"],
+    ["some passage,", "…some passage…"],
+    ["¶Some passage.", "Some passage."],
+  ];
+  it.each(cases)("should format %p as %p", (passage, expected) => {
+    expect(formatPassage(passage)).toBe(expected);
+  });
+});
