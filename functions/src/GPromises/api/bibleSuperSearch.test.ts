4131befddb8b77c94bb3c3db25657e8b6564e743
Mauricio Robayo <rfmajo@gmail.com>
2021-06-14 08:12:42 -0500

    test: add more bibleSuperSearch tests
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- functions/src/GPromises/api/bibleSuperSearch.test.ts
+++ functions/src/GPromises/api/bibleSuperSearch.test.ts
@@ -1,51 +1,57 @@
 import axios from "axios";
 import {bibles} from "../../config";
 import BibleSuperSearch, {ApiResponse} from "./bibleSuperSearch";
-import {translator} from "../utils/translator";
+import {Lang} from "../../types";
 
 const mockVerses = ["verse 1", "verse 2", "verse 3", "verse 4"];
+const mockTranslator = jest.fn((_lang: Lang, reference: string) => reference);
+
+afterEach(() => {
+  mockTranslator.mockClear();
+});
 
 describe("bibleSuperSearch", () => {
   it("should return the correct response for one bible", async () => {
     const requestedBibles: (keyof typeof bibles)[] = ["kjv"];
     const requestedReference = "Book 1:1-2,3; 2:1";
-    const mockResult1 = {
-      verses: {
-        kjv: {
-          "1": {
-            "1": {text: mockVerses[0]},
-            "2": {text: mockVerses[1]},
-          },
-        },
-      },
-    };
-    const mockResult2 = {
-      verses: {
-        kjv: {
-          "1": {
-            "3": {text: mockVerses[2]},
-          },
-        },
-      },
-    };
-    const mockResult3 = {
-      verses: {
-        kjv: {
-          "2": {
-            "1": {text: mockVerses[3]},
-          },
-        },
-      },
-    };
     const mockResponse: ApiResponse = {
-      results: [mockResult1, mockResult2, mockResult3],
+      results: [
+        {
+          verses: {
+            kjv: {
+              "1": {
+                "1": {text: mockVerses[0]},
+                "2": {text: mockVerses[1]},
+              },
+            },
+          },
+        },
+        {
+          verses: {
+            kjv: {
+              "1": {
+                "3": {text: mockVerses[2]},
+              },
+            },
+          },
+        },
+        {
+          verses: {
+            kjv: {
+              "2": {
+                "1": {text: mockVerses[3]},
+              },
+            },
+          },
+        },
+      ],
     };
     const mockedGet = jest.spyOn(axios, "get").mockImplementation(() => {
       return Promise.resolve({
         data: mockResponse,
       });
     });
-    const bibleSuperSearch = new BibleSuperSearch(bibles, translator);
+    const bibleSuperSearch = new BibleSuperSearch(bibles, mockTranslator);
     const content = await bibleSuperSearch.getPassageFromReference(
       requestedBibles,
       requestedReference
@@ -63,5 +69,98 @@ describe("bibleSuperSearch", () => {
         requestedBibles
       )}&reference=${requestedReference}`
     );
+    expect(mockTranslator).toBeCalledTimes(1);
+    requestedBibles.forEach((bible) => {
+      expect(mockTranslator).toBeCalledWith(
+        bibles[bible].lang,
+        requestedReference
+      );
+    });
+  });
+  it("should return the correct response for two bibles", async () => {
+    const requestedBibles: (keyof typeof bibles)[] = ["kjv", "rvg"];
+    const requestedReference = "Book 1:1-2,3; 2:1";
+    const mockResponse: ApiResponse = {
+      results: [
+        {
+          verses: {
+            kjv: {
+              "1": {
+                "1": {text: mockVerses[0]},
+                "2": {text: mockVerses[1]},
+              },
+            },
+            rvg: {
+              "1": {
+                "1": {text: mockVerses[0]},
+                "2": {text: mockVerses[1]},
+              },
+            },
+          },
+        },
+        {
+          verses: {
+            kjv: {
+              "1": {
+                "3": {text: mockVerses[2]},
+              },
+            },
+            rvg: {
+              "1": {
+                "3": {text: mockVerses[2]},
+              },
+            },
+          },
+        },
+        {
+          verses: {
+            kjv: {
+              "2": {
+                "1": {text: mockVerses[3]},
+              },
+            },
+            rvg: {
+              "2": {
+                "1": {text: mockVerses[3]},
+              },
+            },
+          },
+        },
+      ],
+    };
+    const mockedGet = jest.spyOn(axios, "get").mockImplementation(() => {
+      return Promise.resolve({
+        data: mockResponse,
+      });
+    });
+    const bibleSuperSearch = new BibleSuperSearch(bibles, mockTranslator);
+    const content = await bibleSuperSearch.getPassageFromReference(
+      requestedBibles,
+      requestedReference
+    );
+    const expected = {
+      kjv: {
+        text: "verse 1 verse 2 …verse 3 …verse 4",
+        reference: requestedReference,
+      },
+      rvg: {
+        text: "verse 1 verse 2 …verse 3 …verse 4",
+        reference: requestedReference,
+      },
+    };
+
+    expect(content).toEqual(expected);
+    expect(mockedGet).toBeCalledWith(
+      `https://api.biblesupersearch.com/api?bible=${JSON.stringify(
+        requestedBibles
+      )}&reference=${requestedReference}`
+    );
+    expect(mockTranslator).toBeCalledTimes(2);
+    requestedBibles.forEach((bible) => {
+      expect(mockTranslator).toBeCalledWith(
+        bibles[bible].lang,
+        requestedReference
+      );
+    });
   });
 });
