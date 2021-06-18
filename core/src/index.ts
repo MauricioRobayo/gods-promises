e4a483a3e419dab9a845c609141853fb577831db
Mauricio Robayo <rfmajo@gmail.com>
2021-06-17 20:24:32 -0500

    chore: initial core commit
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ core/src/index.ts
@@ -0,0 +1,3 @@
+export * as models from "./models";
+export * as types from "./types";
+export * as config from "./config";
---
+++ core/src/types.ts
@@ -0,0 +1,6 @@
+import { bibles, bibleIds, langs } from "./config";
+
+export type BibleIds = typeof bibleIds;
+export type Bibles = typeof bibles;
+export type BibleId = typeof bibleIds[number];
+export type Lang = typeof langs[number];
