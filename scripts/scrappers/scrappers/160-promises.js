5d8fecfb1c9571a5ed149a043e1155d1a49e9302
Mauricio Robayo <rfmajo@gmail.com>
2021-06-24 18:52:34 -0500

    refactor: use getReferences instead of makePromise
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- scripts/scrappers/scrappers/160-promises.js
+++ scripts/scrappers/scrappers/160-promises.js
@@ -1,26 +1,9 @@
 const fs = require("fs").promises;
 const axios = require("axios");
-const cheerio = require("cheerio");
-const { makePromise, writeData } = require("../helpers");
+const { getReferences, writeData } = require("../helpers");
 
 const url = "https://outuponthewaters.com/160-promises-of-god-from-scripture/";
 axios.get(url).then(({ data }) => {
-  const promises = [];
-  const $ = cheerio.load(data);
-  $(".fl-module-content.fl-node-content").each((_, el) => {
-    const content = $(el);
-    content.find("p").each((_, p) => {
-      const promise = $(p).text();
-      const match = promise.match(/ - (.*)$/);
-      if (match) {
-        const reference = match[1];
-        const promise = makePromise({ reference, source: url });
-        if (promise) {
-          promises.push(promise);
-        }
-      }
-    });
-  });
-
-  writeData(promises, "160-promises.json");
+  const references = getReferences(data, url);
+  writeData(references, "160-promises.json");
 });
