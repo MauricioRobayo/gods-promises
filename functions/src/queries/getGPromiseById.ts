836665648bc45a2e1a46a9e98348a1603848fe0b
Mauricio Robayo <rfmajo@gmail.com>
2021-06-25 16:53:26 -0500

    chore: use config constants
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- functions/src/queries/getGPromiseById.ts
+++ functions/src/queries/getGPromiseById.ts
@@ -1,11 +1,16 @@
 import {getMongoDbCollection} from "../utils";
 import {GPromise, IGPromise} from "@mauriciorobayo/gods-promises/lib/models";
-import {bibles} from "@mauriciorobayo/gods-promises/lib/config";
+import {
+  bibles,
+  G_PROMISES_COLLECTION,
+} from "@mauriciorobayo/gods-promises/lib/config";
 
 export default async function getGPromiseById(
   id: string
 ): Promise<GPromise | null> {
-  const promisesCollection = await getMongoDbCollection<IGPromise>("gPromises");
+  const promisesCollection = await getMongoDbCollection<IGPromise>(
+    G_PROMISES_COLLECTION
+  );
   const promise = await promisesCollection.findOne({_id: id});
   if (!promise) {
     return null;
