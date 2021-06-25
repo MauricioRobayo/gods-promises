23bd8f22a4ae4f5915734b1defb607331332474a
Mauricio Robayo <rfmajo@gmail.com>
2021-06-25 16:52:22 -0500

    chore: update gods-promises to v2.0.2
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- functions/src/queries/getRandomGPromises.ts
+++ functions/src/queries/getRandomGPromises.ts
@@ -1,9 +1,15 @@
-import {bibles} from "@mauriciorobayo/gods-promises/lib/config";
+import {
+  bibles,
+  G_PROMISES_COLLECTION,
+} from "@mauriciorobayo/gods-promises/lib/config";
 import {GPromise, IGPromise} from "@mauriciorobayo/gods-promises/lib/models";
+
 import {getMongoDbCollection} from "../utils";
 
 const getRandomPromises = async (size: number): Promise<GPromise[]> => {
-  const collection = await getMongoDbCollection<IGPromise>("gPromises");
+  const collection = await getMongoDbCollection<IGPromise>(
+    G_PROMISES_COLLECTION
+  );
   const cursor = collection.aggregate<IGPromise>([
     {$match: {failed: {$exists: false}}},
     {$sample: {size}},
