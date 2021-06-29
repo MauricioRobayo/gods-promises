cf471aaf95c499e2cfc6b7fe022aaf8fc312c0fa
Mauricio Robayo <rfmajo@gmail.com>
2021-06-29 16:23:09 -0500

    chore(#46): add one more scrapper
    closes #46
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ scripts/scrappers/src/scrappers/4-sites.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { getReferences, writeData } from "../helpers";
+
+const urls = [
+  "https://www.compassionuk.org/blogs/gods-promises/",
+  "https://www.biblestudytools.com/topical-verses/gods-promises-verses-in-the-bible/",
+  "https://lisaappelo.com/40-promises-of-god-when-you-need-hope/",
+  "https://davidjeremiah.blog/40-bible-verses-for-navigating-gods-promises-and-blessings/",
+];
+
+Promise.all(
+  urls.map(async (url) => {
+    const { data } = await axios.get(url);
+    return getReferences(data, url);
+  })
+).then((references) => {
+  writeData(references.flat(), "4-sites.json");
+});
