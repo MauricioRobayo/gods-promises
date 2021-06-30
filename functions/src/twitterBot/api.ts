29b4ebd926077ae14eabcbd00519bbb22fc88d23
Mauricio Robayo <rfmajo@gmail.com>
2021-06-30 09:53:27 -0500

    chore: add Twitter search functionality
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- functions/src/twitterBot/api.ts
+++ functions/src/twitterBot/api.ts
@@ -1,18 +1,20 @@
 /* eslint-disable camelcase */
 import axios from "axios";
 import * as functions from "firebase-functions";
+import camelCase from "lodash/camelCase";
 export type Tweet = {
   id: string;
   text: string;
 };
+export type Meta = {
+  newestId?: string;
+  oldestId?: string;
+  resultCount: number;
+  nextToken?: string;
+};
 export type RecentSearchResult = {
-  data?: Tweet[];
-  meta: {
-    newest_id?: string;
-    oldest_id?: string;
-    result_count: number;
-    next_token?: string;
-  };
+  tweets: Tweet[];
+  meta: Meta;
 };
 type Options = {
   since_id?: string;
@@ -22,7 +24,15 @@ export async function searchRecent(
   query: string,
   options: Options = {}
 ): Promise<RecentSearchResult> {
-  const {data} = await axios.request<RecentSearchResult>({
+  const {data} = await axios.request<{
+    data?: Tweet[];
+    meta: {
+      newest_id?: string;
+      oldest_id?: string;
+      result_count: number;
+      nextToken?: string;
+    };
+  }>({
     baseURL: "https://api.twitter.com/2/tweets/search/recent",
     params: {
       query,
@@ -32,5 +42,10 @@ export async function searchRecent(
       authorization: `Bearer ${functions.config().twitter.token}`,
     },
   });
-  return data;
+  return {
+    tweets: data.data || [],
+    meta: Object.fromEntries(
+      Object.entries(data.meta).map(([key, value]) => [camelCase(key), value])
+    ) as Meta,
+  };
 }
--- functions/src/twitterBot/retweet.ts
+++ functions/src/twitterBot/retweet.ts
@@ -1,11 +1,40 @@
+import {DocumentReference} from "@google-cloud/firestore";
 import admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import {searchRecent, Tweet} from "./api";
+import {Meta, searchRecent} from "./api";
+const BcvParser =
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require("bible-passage-reference-parser/js/en_bcv_parser").bcv_parser;
+
+const bcv = new BcvParser();
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: "https://promises-edfea.firebaseio.com",
 });
+
+class SearchHistory {
+  private docRef: DocumentReference<FirebaseFirestore.DocumentData>;
+  constructor() {
+    this.docRef = admin.firestore().collection("retweets").doc("GodsPromises");
+  }
+
+  async getLastSearchMeta(): Promise<Meta | undefined> {
+    const doc = await this.docRef.get();
+    const data = doc.data();
+    return data as Meta | undefined;
+  }
+
+  setLastSearchMeta(meta: Meta): Promise<FirebaseFirestore.WriteResult> {
+    return this.docRef.set(meta);
+  }
+}
+
+const searchHistory = new SearchHistory();
+const addHashtagIfMissing = (hashtag: string): string =>
+  hashtag.startsWith("#") ? hashtag : `#${hashtag}`;
 
 export const retweet = functions.pubsub
   .schedule("every 5 minutes")
@@ -13,31 +42,34 @@ export const retweet = functions.pubsub
     retweetHashtag("GodsPromises");
   });
 
-async function retweetHashtag(hashtag: string): Promise<Tweet[]> {
-  const docRef = admin.firestore().collection("retweets").doc("GodsPromises");
-  const lastRetweetInfo = await docRef.get();
-  const lastRetweetId = lastRetweetInfo.data()?.newestId;
-
-  const options = lastRetweetId ? {since_id: lastRetweetId} : {};
-
-  const addHashtagIfMissing = (hashtag: string): string =>
-    hashtag.startsWith("#") ? hashtag : `#${hashtag}`;
+async function retweetHashtag(hashtag: string) {
+  const lastSearchMeta = await searchHistory.getLastSearchMeta();
+  const options = lastSearchMeta?.newestId
+    ? {since_id: lastSearchMeta.newestId}
+    : {};
 
   try {
-    const {meta, data} = await searchRecent(
+    const {meta, tweets} = await searchRecent(
       addHashtagIfMissing(hashtag),
       options
     );
 
-    if (meta.result_count > 0) {
-      docRef.set({newestId: meta.newest_id});
+    if (meta.resultCount === 0) {
+      return;
     }
 
-    return data || [];
+    await searchHistory.setLastSearchMeta(meta);
+
+    tweets.forEach((tweet) => {
+      const osis: string = bcv.parse(tweet.text).osis();
+
+      console.log({text: tweet.text, osis});
+    });
+
+    await searchHistory.setLastSearchMeta(meta);
   } catch (err) {
     console.log(err);
   }
-  return [];
 }
 
-retweetHashtag("GodsPromises").then((result) => console.log({result}));
+retweetHashtag("GodsPromises");
