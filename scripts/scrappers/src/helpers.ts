c645820f8ebaa6089562d7fcd77434bb914bbe6e
Mauricio Robayo <rfmajo@gmail.com>
2021-07-02 12:23:55 -0500

    chore: add pubId while reading files
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- scripts/scrappers/src/helpers.ts
+++ scripts/scrappers/src/helpers.ts
@@ -2,19 +2,39 @@ import { IGPromise } from "@mauriciorobayo/gods-promises/lib/models";
 import dotenv from "dotenv";
 import fs from "fs/promises";
 import uniqBy from "lodash/uniqBy";
+import { GPromisesRepository } from "@mauriciorobayo/gods-promises/lib/repositories";
 dotenv.config();
+
+function idGenerator() {
+  const usedIds = new Set();
+
+  return function () {
+    let id = GPromisesRepository.pubIdGenerator();
+    while (usedIds.has(id)) {
+      console.log(`Id '${id} already used! Getting a new one...`);
+      id = GPromisesRepository.pubIdGenerator();
+    }
+    usedIds.add(id);
+    return id;
+  };
+}
 
 export async function gPromisesFromFiles(
   files: (string | number)[]
 ): Promise<IGPromise[]> {
-  const data = await Promise.all(
+  const getId = idGenerator();
+
+  const data = await Promise.all<Omit<IGPromise, "pubId">>(
     files.map(async (file) => {
       const data = await fs.readFile(String(file), "utf-8");
       return JSON.parse(data);
     })
   );
 
-  return uniqBy(data.flat(), "osis");
+  return uniqBy(data.flat(), "niv").map((gPromise) => ({
+    ...gPromise,
+    pubId: getId(),
+  }));
 }
 
 export async function writeData(
