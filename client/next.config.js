4e53057ec76dc61c48ad97f1c0f5e7c61baf3314
Mauricio Robayo <rfmajo@gmail.com>
2021-07-21 11:51:40 -0500

    feat: add i18n
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const nextTranslate = require("next-translate");
+
+module.exports = nextTranslate({});
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,10 @@
+// import useTranslation from "next-translate/useTranslation";
+import { useRouter } from "next/router";
+
 function Home() {
-  return <h1>Home page</h1>;
+  // const { t } = useTranslation();
+  const { locale } = useRouter();
+  return <h1>{locale}</h1>;
 }
 
 export default Home;
