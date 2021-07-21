a1badbde9e6f43732d3c2443f059663f0ca4bc77
Mauricio Robayo <rfmajo@gmail.com>
2021-07-21 10:05:23 -0500

    chore: add google fonts
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client2/src/pages/_document.js
+++ client2/src/pages/_document.js
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +26,22 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cardo:ital@0;1&family=Lato:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
