7a799ef4c671f14ccccbe5c68b72dd93222410f7
Mauricio Robayo <rfmajo@gmail.com>
2021-07-21 09:52:08 -0500

    chore: add Navbar and Twemoji to Layout
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client2/src/components/Layout/Layout.tsx
+++ client2/src/components/Layout/Layout.tsx
@@ -1,11 +1,53 @@
 import React from "react";
+import styled from "styled-components";
+import { Navbar } from "../Navbar";
+import { Twemoji } from "../Twemoji";
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  height: 100vh;
+`;
+
+const StyledNavbar = styled(Navbar)`
+  margin-bottom: 2rem;
+  position: relative;
+  z-index: 0;
+`;
+
+const Main = styled.main`
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr;
+  place-items: center;
+  overflow: hidden;
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  padding: 1rem 0;
+  a {
+    color: ${({ theme }) => theme.color.text2};
+  }
+`;
 
 type LayoutProps = {
   children?: React.ReactNode;
 };
-
 function Layout({ children }: LayoutProps) {
-  return <div>{children}</div>;
+  return (
+    <Wrapper>
+      <StyledNavbar />
+      <Main>{children}</Main>
+      <Footer>
+        <a href="https://godspromises.bible">
+          <Twemoji emoji="🙏" /> God&apos;s Promises
+        </a>
+      </Footer>
+    </Wrapper>
+  );
 }
 
 export default Layout;
---
+++ client2/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import { Twemoji } from "../Twemoji";
+
+const StyledNavbar = styled.nav`
+  display: flex;
+  padding: 0.5rem;
+  justify-content: center;
+  @media ${({ theme }) => theme.device.small} {
+    padding: 1rem;
+  }
+  border-bottom: 2px solid ${({ theme }) => theme.color.surface3};
+`;
+
+const NavbarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: ${({ theme }) => theme.size.medium};
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 1.25rem;
+
+  @media ${({ theme }) => theme.device.medium} {
+    font-size: 1.75rem;
+  }
+  margin: 0;
+  font-weight: 900;
+  a {
+    color: ${({ theme }) => theme.color.text1};
+  }
+`;
+
+type NavbarProps = {
+  className?: string;
+};
+function Navbar({ className = "" }: NavbarProps) {
+  return (
+    <StyledNavbar className={className}>
+      <NavbarWrapper>
+        <Title>
+          <a href="https://godspromises.bible">
+            <Twemoji emoji="🙏" /> God&apos;s Promises
+          </a>
+        </Title>
+      </NavbarWrapper>
+    </StyledNavbar>
+  );
+}
+
+export default Navbar;
