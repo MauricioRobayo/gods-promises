abe00b36ab9b2bdbbfc25ef5ba9bfd7677da55c1
Mauricio Robayo <rfmajo@gmail.com>
2021-07-22 06:01:53 -0500

    chore(client): update types
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/src/hooks/useGPromise.ts
+++ client/src/hooks/useGPromise.ts
@@ -1,10 +1,14 @@
 import { useQuery } from "react-query";
 import { firebaseCallable } from "../services/firebase";
-import { IGPromise } from "@mauriciorobayo/gods-promises/lib/models";
+import { GPromiseDTO } from "@mauriciorobayo/gods-promises/lib/models";
 
 export default function useGPromise(gPromiseId: string) {
-  const getPromiseById = firebaseCallable<IGPromise, string>("promise");
-  return useQuery(["promise", gPromiseId], () => getPromiseById(gPromiseId), {
-    staleTime: Infinity,
-  });
+  const getPromiseById = firebaseCallable<GPromiseDTO, string>("promise");
+  return useQuery<GPromiseDTO>(
+    ["promise", gPromiseId],
+    () => getPromiseById(gPromiseId),
+    {
+      staleTime: Infinity,
+    }
+  );
 }
