d3f6cf55924e0e919e2dd2032a047007667857b5
Mauricio Robayo <rfmajo@gmail.com>
2021-07-22 16:59:22 -0500

    chore: server side rendering of current promise
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/src/pages/p/[id].tsx
+++ client/src/pages/p/[id].tsx
@@ -1,4 +1,3 @@
-import useGPromise from "../../hooks/useGPromise";
 import useRandomGPromise from "../../hooks/useRandomGPromise";
 import { AppLoader } from "../../components/Loader";
 import Link from "next/link";
@@ -19,36 +18,48 @@ import {
 import useTranslation from "next-translate/useTranslation";
 import { useRouter } from "next/router";
 import { localeInfo } from "../../config";
-import { useQueryClient } from "react-query";
 import React, { useEffect } from "react";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { useQueryClient } from "react-query";
+import axios from "axios";
 
-export default function GPromise() {
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  if (typeof params?.id !== "string") {
+    throw new Error();
+  }
+  const { data } = await axios.post(
+    "http://127.0.0.1:5001/promises-edfea/us-central1/promise",
+    {
+      data: params.id,
+    }
+  );
+  return {
+    props: {
+      data: data.result,
+    },
+  };
+};
+
+export default function GPromise({
+  data, // TODO: #75 fix type of data
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { t } = useTranslation("common");
-  const { t: tError } = useTranslation("error");
   const router = useRouter();
 
   const locale = router.locale as string;
   const gPromiseId = router.query.id as string;
 
   const { bibleId } = localeInfo[locale];
-  const gPromiseQuery = useGPromise(gPromiseId);
   const randomGPromiseQuery = useRandomGPromise();
   const queryClient = useQueryClient();
 
   useEffect(() => {
+    // TODO: #76 promise Id is the same as the current page
     queryClient.refetchQueries("randomGPromise");
   }, [queryClient]);
 
-  if (gPromiseQuery.isLoading || gPromiseQuery.isIdle) {
-    return <AppLoader />;
-  }
-
-  if (gPromiseQuery.isError || randomGPromiseQuery.isError) {
-    return <div>{tError("generic error")}</div>;
-  }
-
   const baseUrl = "https://godspromises.bible";
-  const { text, reference, bibleName } = gPromiseQuery.data.content[bibleId];
+  const { text, reference, bibleName } = data.content[bibleId];
   const title = `${reference} | ${t("God's Promises")}`;
   const description = `${text} ${reference}`;
   const link = `${baseUrl}${router.asPath}`;
@@ -61,19 +72,20 @@ export default function GPromise() {
     });
   };
 
-  const shareButton =
-    "share" in navigator ? (
-      <ShareButton type="button" onClick={share} title={t("Share")}>
-        <ShareIcon />
-      </ShareButton>
-    ) : (
-      <a
-        title="Tweet"
-        href={`https://twitter.com/intent/tweet?text=${description} ${link}`}
-      >
-        <TwitterIcon />
-      </a>
-    );
+  const hasShare = typeof navigator !== "undefined" && "share" in navigator;
+
+  const shareButton = hasShare ? (
+    <ShareButton type="button" onClick={share} title={t("Share")}>
+      <ShareIcon />
+    </ShareButton>
+  ) : (
+    <a
+      title="Tweet"
+      href={`https://twitter.com/intent/tweet?text=${description} ${link}`}
+    >
+      <TwitterIcon />
+    </a>
+  );
 
   return (
     <Article>
