b483cc28df83737fafbbcb8ba484609897c11606
Mauricio Robayo <rfmajo@gmail.com>
2021-07-22 10:26:46 -0500

    chore(client): add config
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,1 +1,7 @@
-export const PROMISE_PATH = "p";
+import { BibleId } from "@mauriciorobayo/gods-promises/lib/types";
+
+export type Locale = "en" | "es";
+export const localeInfo: Record<string, { name: string; bibleId: BibleId }> = {
+  en: { name: "English", bibleId: "kjv" },
+  es: { name: "Español", bibleId: "rvg" },
+};
