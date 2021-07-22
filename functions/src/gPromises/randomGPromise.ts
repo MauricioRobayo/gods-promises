f7ec2069d195a048c8bb821a1781382e12275edf
Mauricio Robayo <rfmajo@gmail.com>
2021-07-22 10:13:27 -0500

    fix(functions): throw error instead of returning error object
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- functions/src/gPromises/randomGPromise.ts
+++ functions/src/gPromises/randomGPromise.ts
@@ -2,13 +2,14 @@ import * as functions from "firebase-functions";
 import {updateMissingContent} from "./utils";
 import {GPromisesRepository} from "@mauriciorobayo/gods-promises/lib/repositories";
 import {GPromiseDTO} from "@mauriciorobayo/gods-promises/lib/models";
+import {HttpsError} from "firebase-functions/lib/providers/https";
 
 const gPromisesRepository = new GPromisesRepository(
   functions.config().mongodb.uri
 );
 
 export const randomGPromise = functions.https.onCall(
-  async (): Promise<GPromiseDTO | {error: string}> => {
+  async (): Promise<GPromiseDTO> => {
     const randomGPromises = await gPromisesRepository.getRandomPromises(100);
 
     for (const randomGPromise of randomGPromises) {
@@ -32,8 +33,6 @@ export const randomGPromise = functions.https.onCall(
       }
     }
 
-    return {
-      error: "Could not get a random promise",
-    };
+    throw new HttpsError("internal", "Could not find a random promise");
   }
 );
