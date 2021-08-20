4783c003b9ed2b78f9a24d1a05d2c1a826a9321f
Mauricio Robayo <rfmajo@gmail.com>
2021-08-19 20:57:37 -0500

    chore: use switch-emoji
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/src/components/Twemoji/Twemoji.tsx
+++ client/src/components/Twemoji/Twemoji.tsx
@@ -1,41 +1,13 @@
-import React, { memo } from "react";
-import styled from "styled-components";
+import { Semoji } from "switch-emoji";
 
-type TwemojiProps = {
+type Props = {
   emoji: string;
   height?: string;
-  width?: string;
   className?: string;
 };
 
-const Wrapper = styled.span`
-  line-height: 0;
-`;
-
-const StyledImg = styled.img<Pick<TwemojiProps, "height" | "width">>`
-  display: inline-block;
-  height: ${({ height }) => height};
-  width: ${({ width }) => width};
-`;
-
-const Twemoji = ({
-  emoji,
-  className = "",
-  height = "1em",
-  width = "auto",
-}: TwemojiProps) => {
-  const emojiCode = emoji.codePointAt(0)?.toString(16);
-  return (
-    <Wrapper>
-      <StyledImg
-        className={className}
-        src={`https://twemoji.maxcdn.com/v/latest/svg/${emojiCode}.svg`}
-        alt={emoji}
-        height={height}
-        width={width}
-      />
-    </Wrapper>
-  );
+const Twemoji = (props: Props) => {
+  return <Semoji {...props} source="Twitter" />;
 };
 
-export default memo(Twemoji);
+export default Twemoji;
