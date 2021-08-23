08cb9f5be86d4c0972851cb59dec6ac5152a9b60
Mauricio Robayo <rfmajo@gmail.com>
2021-08-23 08:11:28 -0500

    feat(#73): add feedback link
    closes #73
    
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,25 +1,33 @@
 import Link from "next/link";
+import { Email } from "react-obfuscate-email";
 import styled from "styled-components";
 import Logo from "../Logo/Logo";
 
 const StyledFooter = styled.footer`
-  display: flex;
-  align-items: flex-end;
-  justify-content: center;
+  display: grid;
+  place-items: center;
   padding: 1rem 0;
   a {
     color: ${({ theme }) => theme.color.text2};
+  }
+  p {
+    margin: 0.5rem 0;
   }
 `;
 
 function Footer() {
   return (
     <StyledFooter>
-      <Link href="/">
-        <a>
-          <Logo />
-        </a>
-      </Link>
+      <p>
+        <Link href="/">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+      </p>
+      <p>
+        <Email email="info@godspromises.bible" />
+      </p>
     </StyledFooter>
   );
 }
