9a5ccea914e00fd78fadffa13bc1d0ae8027f12d
Giraficode <giraficode@gmail.com>
2021-10-17 07:02:28 +0000

    add test

--- functions/src/gPromises/utils/formatPassage.test.ts
+++ functions/src/gPromises/utils/formatPassage.test.ts
@@ -1,4 +1,4 @@
-import {formatPassage} from "./formatPassage";
+import { formatPassage } from "./formatPassage"
 
 describe("formatPassage", () => {
   const cases = [
@@ -17,3 +17,54 @@ describe("formatPassage", () => {
     expect(formatPassage(text)).toBe(expected);
   });
 });
+
+// @ponicode
+describe("formatPassage.formatPassage", () => {
+    test("0", () => {
+        let callFunction: any = () => {
+            formatPassage.formatPassage("   123¶user-name")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("1", () => {
+        let callFunction: any = () => {
+            formatPassage.formatPassage("¶ ")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("2", () => {
+        let callFunction: any = () => {
+            formatPassage.formatPassage("¶")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("3", () => {
+        let callFunction: any = () => {
+            formatPassage.formatPassage(" 123¶user-name")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("4", () => {
+        let callFunction: any = () => {
+            formatPassage.formatPassage("    123¶user-name")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+
+    test("5", () => {
+        let callFunction: any = () => {
+            formatPassage.formatPassage("")
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
